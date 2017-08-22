4ad34a4d19c6143132105f39b64f9cc7c7d052db
mstclair <stclairm@spu.edu>
2017-08-22 14:32:48 -0700

    finished to-dos and stretch goal

--- starter-code/server.js
+++ starter-code/server.js
@@ -2,7 +2,7 @@
 
 // DONE: Initialize your project using NPM to create and populate a package.json file
 
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// DONE: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
@@ -13,12 +13,12 @@ const app = express();
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
+// DONE: Include all of the static resources as an argument to app.use()
 app.use(express.static('./public'));
 
 
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-app.get('/new', function(req, resp){
+// DONE: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+  app.get('/new', function(req, resp){
   console.log('new page');
   resp.sendFile('public/new.html', {root: '.'});
 });
